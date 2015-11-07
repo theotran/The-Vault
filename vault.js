a62b2a69165b617006a37b100b1b51056a785583
theo tran <theotran@rocketmail.com>
2015-11-07 11:49:52 -1000

    Passed all tests, project completed

--- vault.js
+++ vault.js
@@ -1,4 +1,50 @@
 'use strict';
 module.exports = function() {
+	
+
+	var getValue = function (key) {
+	  	if (myVault.hasOwnProperty("key") === true) {
+	  		return myVault.key;
+
+	  	} else {
+	  		return null;
+	  	}
+
+	  	
+  	};
+
+  	var setValue = function (key, value) {
+  		//if (myVault.hasOwnProperty("key") === true) {
+	  		myVault.key = value;
+	  		
+	  	//} 
+  	
+  	};
+
+
+	var myVault = {
+		"setValue": setValue,
+		"getValue": getValue,
+		 
+	};
+
+	return myVault;
   
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+  
